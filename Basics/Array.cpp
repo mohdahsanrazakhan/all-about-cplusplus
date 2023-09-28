d2ed885e133a24382d31681e087a5592a128cd1e
Mark <datawithmark@gmail.com>
2023-09-28 21:25:58 +0530

    2D Matrix

--- Basics/Array.cpp
+++ Basics/Array.cpp
@@ -124,3 +124,28 @@ int main() {
 
 	return 0;
 }
+
+// To-Do: Get input from user and print.
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int r, c;
+	cin >> r >> c;
+
+	int arr[r][c];
+
+	for(int i = 0; i < r; i++) {
+		for(int j = 0; j < c; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	for(int i = 0; i < r; i++) {
+		for(int j = 0; j < c; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
