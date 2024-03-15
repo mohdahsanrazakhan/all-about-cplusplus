c4eaa10793cd938a01673e184a2162ce711e0674
Mark <datawithmark@gmail.com>
2024-03-15 23:14:28 +0530

    Optimal Approach of Problem 03

--- TUF Sheet/Arrays/Easy/problem_03.cpp
+++ TUF Sheet/Arrays/Easy/problem_03.cpp
@@ -1,6 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Brute Force Approach -> TC: O(N^2) | SC: O(1)
+bool isSortedBrute(int arr[], int n) {
+	
+	for(int i = 0; i < n; i++) {
+		for(int j = i + 1; j < n; j++) {
+			if(arr[i] > arr[j]) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+// Better Approach -> TC: O(N) | SC: O(1)
 bool isSorted(int arr[], int n) {
 	// Edge case
 	if(n < 2) 
@@ -22,8 +36,21 @@ bool isSorted(int arr[], int n) {
 	return flag;
 }
 
+// Optimal Approach -> TC: O(N) | SC: O(1)
+bool isSortedOptimal(int arr[], int n) {
+	
+	for(int i = 1; i < n; i++) {
+		if(arr[i] < arr[i-1]) {
+			return false;
+		}
+	}
+	return true;
+}
+
 int main() {
 	int arr[] = {5, 2, 3, 4, 5};
 	int n = sizeof(arr)/sizeof(arr[0]);
-	cout << isSorted(arr, n);
+	cout << isSortedBrute(arr, n) << endl;
+	cout << isSorted(arr, n) << endl;
+	cout << isSortedOptimal(arr, n) << endl;
 }
