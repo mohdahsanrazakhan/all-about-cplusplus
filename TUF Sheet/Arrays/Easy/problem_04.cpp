46afde0445183fd24decce77be82d89bc4ab4835
Mark <datawithmark@gmail.com>
2024-03-16 23:40:52 +0530

    Other approch added Problem 4

--- TUF Sheet/Arrays/Easy/problem_04.cpp
+++ TUF Sheet/Arrays/Easy/problem_04.cpp
@@ -1,7 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// Better Approach -> TC: O(N) | SC: O(N) 
+// Brute Force Approach -> TC: O(N*log(N))+O(N) | SC: O(N)
+int removeDuplicateBrute(int arr[], int n) {
+	
+	set<int> set;
+	
+	for(int i = 0; i < n; i++) {
+		set.insert(arr[i]);
+	}
+	int sz = set.size();
+	int j = 0;
+	for(int st: set) {
+		arr[j++] = st;
+	}
+	return sz;
+}
+
+// Better Approach -> TC: O(N) | SC: O(N) (Note: Need to Fix)
 int removeDuplicate(int arr[], int n) {
 	
 	vector<int> unique;
@@ -18,8 +34,35 @@ int removeDuplicate(int arr[], int n) {
 	return unique.size();
 }
 
+// Optimal Approach -> TC: (Note: Need to Fix)
+int removeDuplicateOptimal(int arr[], int n) {
+	
+	int i = 0;
+	for (int j = 1; j < n; j++) {
+	    if (arr[i] != arr[j]) {
+	      i++;
+	      arr[i] = arr[j];
+	    }
+	}
+	return i + 1;
+}
+
 int main() {
-	int arr[] = {1,2,2,2,3};
+	int arr[] = {1,1,2,2,2,3,3};
 	int n = sizeof(arr)/sizeof(arr[0]);
-	cout << removeDuplicate(arr, n);
+	int rdb = removeDuplicateBrute(arr, n);
+	int rd = removeDuplicate(arr, n);
+	int rdo = removeDuplicateOptimal(arr, n);
+	for(int i = 0; i < rdb; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	for(int j = 0; j < rd; j++) {
+		cout << arr[j] << " ";
+	}
+	cout << endl;
+	for(int k = 0; k < rdo; k++) {
+		cout << arr[k] << " ";
+	}
+	cout << endl;
 }
