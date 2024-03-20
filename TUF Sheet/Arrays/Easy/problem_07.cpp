f53f59abed2a3fd8a86dfc754bd66a1dd82fa789
Mark <datawithmark@gmail.com>
2024-03-20 11:20:07 +0530

    Problem 07

---
+++ TUF Sheet/Arrays/Easy/problem_07.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Brute Force Approach -> TC: O(N) + O(X) + O(N-X) ~ O(2*N) | SC: O(N)
+void moveZeroToEndBrute(int arr[], int n) {
+	
+	vector<int> temp;
+	
+	for(int i = 0; i < n; i++) {
+		if(arr[i] != 0)
+			temp.push_back(arr[i]); 
+	}
+	
+	int nzSize = temp.size();
+	
+	for(int i = 0; i < nzSize; i++) {
+		arr[i] = temp[i];
+	}
+	
+	for(int i = nzSize; i < n; i++) {
+		arr[i] = 0;
+	}
+	
+}
+
+// Better Approach -> TC: O(N log N) | SC: O(1)
+void moveZeroToEndBetter(int arr[], int n) {
+	
+	sort(arr, arr+n, greater<int>());
+	
+}
+
+int main() {
+	int arr[] = {1, 0, 2, 3, 2, 0, 0, 4, 5, 1};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	
+	moveZeroToEndBrute(arr, n);
+	for(int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	
+	moveZeroToEndBetter(arr, n);
+	for(int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	
+}
