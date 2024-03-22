9d7e84eae66f3e37f1145828f8e49b50cf13cd28
Mark <datawithmark@gmail.com>
2024-03-22 11:15:22 +0530

    Final commit of Problem 09

--- TUF Sheet/Arrays/Easy/problem_09.cpp
+++ TUF Sheet/Arrays/Easy/problem_09.cpp
@@ -37,6 +37,40 @@ vector<int> unionTwoArraysSet(int arr1[], int arr2[], int m, int n) {
 	return Union;
 }
 
+// Using Two Pointer -> TC: O(m+n) | SC: O(1) 
+vector<int> unionTwoArraysPointer(int arr1[], int arr2[], int m, int n) {
+	
+	int i = 0, j = 0;
+	vector<int> Union;
+	
+	while(i < m && j < n) {
+		// Case 1 and 2
+		if(arr1[i] < arr2[j]) {
+			if(Union.size() == 0 || Union.back() != arr1[i])
+				Union.push_back(arr1[i]);
+			i++;			
+		}
+		// Case 3
+		else {
+			if(Union.size() == 0 || Union.back() != arr2[j])
+				Union.push_back(arr2[j]);
+			j++;
+		}
+	}
+	// If any elements left in arr1
+	while(i < m) {
+		if(Union.back() != arr1[i])
+			Union.push_back(arr1[i]);
+		i++;
+	}
+	while(j < n) {
+		if(Union.back() != arr2[j])
+			Union.push_back(arr2[j]);
+		j++;
+	}
+	return Union;
+}
+
 int main() {
 	int arr1[] = {1,2,3,4,5,6,7,8,9,10};
 	int arr2[] = {2,3,4,4,5,11,12};
@@ -54,6 +88,13 @@ int main() {
 	cout << "Union of arr1 and arr2 using SET is ";
 	for(auto &uValS : UnionSet) {
 		cout << uValS << " ";
+	}
+	cout << endl;
+	
+	vector<int> UnionPointers = unionTwoArraysPointer(arr1, arr2, m, n);
+	cout << "Union of arr1 and arr2 using Two Pointers is ";
+	for(auto &uValP : UnionPointers) {
+		cout << uValP << " ";
 	}
 	
 	return 0;
